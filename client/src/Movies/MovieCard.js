264ddb1769c7cf26b4d950a81ed882779dcfb4e2
Farid Hamida <faridhamida22@gmail.com>
2019-12-18 16:27:23 -0800

    updates

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+
 const MovieCard = props => {
   const { title, director, metascore, stars } = props.movie;
+
   return (
     <div className="movie-card">
       <h2>{title}</h2>
@@ -12,7 +14,6 @@ const MovieCard = props => {
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
-
       {stars.map(star => (
         <div key={star} className="movie-star">
           {star}
@@ -21,5 +22,4 @@ const MovieCard = props => {
     </div>
   );
 };
-
 export default MovieCard;
